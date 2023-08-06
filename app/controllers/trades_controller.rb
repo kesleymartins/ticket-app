7b5523786f09bc3557f9e504834d9379f7c204b1
kesleymartins <kesley.martins@vivaldi.net>
2023-08-06 17:45:05 -0300

    finish base operations

---
+++ app/controllers/trades_controller.rb
@@ -0,0 +1,34 @@
+class TradesController < ApplicationController
+  before_action :authenticate_user!
+  
+  def index
+    @trades = Trade.where(status: TradeStatus::OPEN).order(created_at: :desc)
+  end
+
+  def new
+    @trade = Trade.new
+  end
+
+  def create
+    @trade = Trade.new(trade_params)
+
+    if @trade.save
+      turbo_stream
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def close
+    @trade = Trade.find(params[:id])
+    @trade.update({ status: TradeStatus::CLOSED })
+
+    turbo_stream
+  end
+
+  private
+
+  def trade_params
+    params.require(:trade).permit(:have, :wanted, :place)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
-  root "pages#home"
+  
+  root "trades#index"
+
+  resources :trades, only: [:index, :new, :create] do
+    member do
+      put 'close'
+    end
+  end
 end
