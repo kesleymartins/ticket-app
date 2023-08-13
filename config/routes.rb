0bccf6683d81d100ade7e73938ef2deb0492fea4
kesleymartins <kesley.martins@vivaldi.net>
2023-08-12 22:07:58 -0300

    add customized registration route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
-  
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
+
   root "trades#index"
 
   resources :trades, only: [:index, :new, :create] do
