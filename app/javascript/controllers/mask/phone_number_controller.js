c962eda15518e2151214b7655bb54dbac731667a
kesleymartins <kesley.martins@vivaldi.net>
2023-08-12 22:33:39 -0300

    add phone-number controller

---
+++ app/javascript/controllers/mask/phone_number_controller.js
@@ -0,0 +1,13 @@
+import { Controller } from "@hotwired/stimulus"
+import Cleave from 'cleave.js'
+
+// Connects to data-controller="mask--phone-number"
+export default class extends Controller {
+  connect() {
+    new Cleave('.phone-number', {
+      delimiters: ['(', ') ', ' ', '-'],
+      blocks: [0, 3, 1, 4, 4],
+      uppercase: true
+    })
+  }
+}
